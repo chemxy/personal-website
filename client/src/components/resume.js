81c11e512ef3e8fd12c925baafcf818ca6b8b80f
chemxy <44486666+chemxy@users.noreply.github.com>
2021-06-24 22:18:37 -0700

    Update resume.js
    finalize the skeleton of resume page.

--- client/src/components/resume.js
+++ client/src/components/resume.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 import Heading from './heading';
 
 class Work extends React.Component {
     render() {
         return (
-           <></>
+            <Container className="border border-dark work">
+                <h3 className="text-capitalize">{this.props.tittle}</h3>
+                <h5 className=" text-uppercase font-weight-normal font-italic">{this.props.company}</h5>
+                <h5 className="text-capitalize font-weight-normal font-italic">{this.props.location}</h5>
+                <h6 className="font-weight-normal">{this.props.duration}</h6>
+                <p className="text-left">
+                    {this.props.description}
+                </p>
+                <hr />
+            </Container>
         )
     }
 };
@@ -22,62 +34,42 @@ class Resume extends React.Component {
     render() {
         return (
             <>
-                <Heading content="Work experience" />
+                <style>
+                    {`
+                        .work{
+                            background-color: white;
+                            margin-bottom:40px;
+                            padding:30px}
+                    `}
+                </style>
+                <Container>
+                    <Row>
 
-                <div class="main">
-                    <div class="container">
-                        <div class="row">
+                        <Col fluid>
+                            <Heading content="Work experience" />
 
-                            <div class="col-sm-8">
-                              
-                                <div class=" container ">
-                                    <div id="work-exp-2">
-                                        <h3 class="text-capitalize">Product Designer & Developer Co-op</h3>
-                                        <h5 class="font-weight-normal">FROMASTERA DESIGN LTD, Vancouver, BC</h5>
-                                        <h6 class="font-weight-normal font-italic">Sep 2020 – Jan 2021 </h6>
-                                        <p>
-                                            <li>
-                                                Designed and implemented hardware products related to embedded systems such as full
-                                                electronic circuits.
-                                            </li>
-                                            <li>
-                                                Developed and implemented relevant software products such as control systems and
-                                                bluetooth support applications.
-                                            </li>
-                                            <li>Main technologies and programming languages used are C, JavaScript, XML and Kotlin.
-                                            </li>
-                                        </p>
-                                    </div>
-                                    <hr />
-                                    <br />
-                                </div>
+                            <Work tittle="Software develoepr (Co-op)" company="insurance corporation of british columbia (icbc)"
+                                location="Victoria, BC, Canada" duration="Jan 2021 - Dec 2021"
+                                description=" To be continued.
+                            " />
 
-                                <div class=" container ">
-                                    <div id="work-exp-1">
-                                        <h3 class="text-capitalize">software developer co-op</h3>
-                                        <h5 class="font-weight-normal">LIJIN TECHNOLOGY DEVELOPMENT CO., LTD, China</h5>
-                                        <h6 class="font-weight-normal font-italic"> May 2020 – Sep 2020</h6>
-                                        <p>
-                                            <li>
-                                                Designed and developed a web-based Wechat mini-program from scratch and connected it
-                                                to the Wechat cloud database.
-                                            </li>
-                                            <li>
-                                                Main technologies used are Wechat developer tools, WXML, WXSS, JSON and JavaScript.
-                                            </li>
-                                        </p>
-                                    </div>
-                                    <hr />
-                                    <br />
-                                </div>
+                            <Work tittle="Product Designer and Developer (Co-Op)" company="FROMASTERA design ltd"
+                                location=" Vancouver, BC, Canada" duration="Sep 2020 - Dec 2020"
+                                description=" Designed and implemented hardware products related to embedded systems such as full electronic circuits.
+                            Developed and implemented relevant software products such as control systems and bluetooth support applications.
+                            Main technologies and programming languages used are C, JavaScript, XML and Kotlin.
+                            " />
 
+                            <Work tittle="Software Developer (Co-Op)" company="LIJIN TECHNOLOGY DEVELOPMENT CO., LTD"
+                                location="China" duration="May 2020 – Aug 2020"
+                                description=" Designed and developed a web-based Wechat mini-program from scratch and connected it to the Wechat cloud database.
+                            Main technologies used are Wechat developer tools, WXML, WXSS, JSON and JavaScript.                            
+                            " />
 
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
+                        </Col>
+                      
+                    </Row>
+                </Container>
 
             </>
         );
